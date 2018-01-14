8d08bd9745afb88c013cff6417d905724e9e15a7
numb86 <86numb@gmail.com>
2018-01-14 11:15:22 +0900

    30 kyuyo.ml 完成。

--- kyuyo.ml
+++ kyuyo.ml
@@ -1,6 +1,12 @@
+(* 時給（円） *)
+let jikyu = 950
+
+(* 基本給（円） *)
+let kihonkyu = 100
+
 (* 目的：働いた時間 x に応じたアルバイト代を計算する *)
 (* kyuyo : int -> int *)
-let kyuyo x = 0
+let kyuyo x = kihonkyu + x * jikyu
 
 (* テスト *)
 let test1 = kyuyo 25 = 23850
