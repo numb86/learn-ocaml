aeba06adecccb3a8bedc65b38acd3a22a9fb2161
numb86 <86numb@gmail.com>
2018-01-20 11:10:23 +0900

    37 重複していた処理をまとめた。

--- chapter5/kyuyo.ml
+++ chapter5/kyuyo.ml
@@ -10,8 +10,7 @@ let kihonkyu = 100
 (* 目的：働いた時間 x に応じたアルバイト代を計算する *)
 (* kyuyo : int -> int *)
 let kyuyo x =
-  if x < 30 then kihonkyu + x * jikyu
-  else kihonkyu + x * yugu_jikyu
+  kihonkyu + x * (if x < 30 then jikyu else yugu_jikyu)
 
 (* テスト *)
 let test1 = kyuyo 25 = 23850
