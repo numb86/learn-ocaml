cef18a594c8439388eb1996212e750e8fc646354
numb86 <86numb@gmail.com>
2018-03-03 20:15:00 +0900

    8.5の演習問題を解いた。

---
+++ chapter8/q8-1.ml
@@ -0,0 +1,31 @@
+type book_t = {
+  title: string;
+  author: string;
+  publisher: string;
+  price: int;
+  isbn: string
+}
+
+let book1 = {
+  title = "programming";
+  author = "tom";
+  publisher = "o'reilly";
+  price = 500;
+  isbn = "1-1-1";
+}
+
+let book2 = {
+  title = "ocmal-master";
+  author = "tom";
+  publisher = "o'reilly";
+  price = 500;
+  isbn = "1-1-2";
+}
+
+let book3 = {
+  title = "excel";
+  author = "bob";
+  publisher = "o'reilly";
+  price = 500;
+  isbn = "1-2-1";
+}
