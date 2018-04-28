692e58ac68009da0b71d20cc6e4ea251819282d3
numb86 <86numb@gmail.com>
2018-04-28 16:37:57 +0900

    109 fac.ml を定義。

---
+++ chapter11/fac.ml
@@ -0,0 +1,12 @@
+(* 目的：自然数 n の階乗を求める *)
+(* fac: int -> int *)
+let rec fac n = if n = 0
+  then 1
+  else n * fac (n - 1)
+
+let test1 = fac 0 = 1
+let test2 = fac 1 = 1
+let test3 = fac 2 = 2
+let test4 = fac 3 = 6
+let test5 = fac 4 = 24
+let test6 = fac 10 = 3628800
