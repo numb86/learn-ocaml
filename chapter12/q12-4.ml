f29483fa777b243b1743ac14702142e0bd561a6a
numb86 <86numb@gmail.com>
2018-04-30 13:46:42 +0900

    12.5の演習問題を解いた。

---
+++ chapter12/q12-4.ml
@@ -0,0 +1,64 @@
+type ekimei_t = { 
+  kanji   : string; (* 駅名 *) 
+  kana    : string; (* 読み *) 
+  romaji  : string; (* ローマ字 *) 
+  shozoku : string; (* 所属線名 *) 
+} 
+
+(* 駅名リストの例 *) 
+ 
+let ekimei_list = [ 
+{kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="丸ノ内線"}; 
+{kanji="新大塚"; kana="しんおおつか"; romaji="shinotsuka"; shozoku="丸ノ内線"}; 
+{kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸ノ内線"}; 
+{kanji="後楽園"; kana="こうらくえん"; romaji="korakuen"; shozoku="丸ノ内線"}; 
+{kanji="本郷三丁目"; kana="ほんごうさんちょうめ"; romaji="hongosanchome"; shozoku="丸ノ内線"}; 
+{kanji="御茶ノ水"; kana="おちゃのみず"; romaji="ochanomizu"; shozoku="丸ノ内線"} 
+] 
+ 
+(* 目的：昇順に並んでいる lst の正しい位置に ekimei を挿入する *) 
+(* ekimei_insert : ekimei_t list -> ekimei_t -> ekimei_t list *) 
+let rec ekimei_insert lst ekimei0 = match lst with 
+    [] -> [ekimei0] 
+  | ({kanji = k; kana = a; romaji = r; shozoku = s} as ekimei) :: rest -> 
+      match ekimei0 with {kanji = k0; kana = a0; romaji = r0; shozoku = s0} -> 
+	if a = a0 then ekimei_insert rest ekimei0 
+	else if a < a0 then ekimei :: ekimei_insert rest ekimei0 
+	else ekimei0 :: lst 
+ 
+(* テスト *) 
+let test1 = ekimei_insert [] {kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸ノ内線"} 
+	    = [{kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸ノ内線"}] 
+let test2 = ekimei_insert [ 
+	{kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="丸ノ内線"}; 
+	{kanji="御茶ノ水"; kana="おちゃのみず"; romaji="ochanomizu"; shozoku="丸ノ内線"}; 
+	{kanji="新大塚"; kana="しんおおつか"; romaji="shinotsuka"; shozoku="丸ノ内線"}; 
+	{kanji="本郷三丁目"; kana="ほんごうさんちょうめ"; romaji="hongosanchome"; shozoku="丸ノ内線"}; 
+	{kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸ノ内線"} 
+	] 
+	{kanji="後楽園"; kana="こうらくえん"; romaji="korakuen"; shozoku="丸ノ内線"} 
+= [ 
+{kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="丸ノ内線"}; 
+{kanji="御茶ノ水"; kana="おちゃのみず"; romaji="ochanomizu"; shozoku="丸ノ内線"}; 
+{kanji="後楽園"; kana="こうらくえん"; romaji="korakuen"; shozoku="丸ノ内線"}; 
+{kanji="新大塚"; kana="しんおおつか"; romaji="shinotsuka"; shozoku="丸ノ内線"}; 
+{kanji="本郷三丁目"; kana="ほんごうさんちょうめ"; romaji="hongosanchome"; shozoku="丸ノ内線"}; 
+{kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸ノ内線"} 
+] 
+ 
+(* 目的：ekimei list をひらがなの順に整列しながら駅の重複を取り除く *) 
+(* seiretsu : ekimei_t list -> ekimei_t list *) 
+let rec seiretsu ekimei_list = match ekimei_list with 
+    [] -> [] 
+  | first :: rest -> ekimei_insert (seiretsu rest) first 
+ 
+(* テスト *) 
+let test3 = seiretsu [] = [] 
+let test4 = seiretsu ekimei_list = [ 
+{kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="丸ノ内線"}; 
+{kanji="御茶ノ水"; kana="おちゃのみず"; romaji="ochanomizu"; shozoku="丸ノ内線"}; 
+{kanji="後楽園"; kana="こうらくえん"; romaji="korakuen"; shozoku="丸ノ内線"}; 
+{kanji="新大塚"; kana="しんおおつか"; romaji="shinotsuka"; shozoku="丸ノ内線"}; 
+{kanji="本郷三丁目"; kana="ほんごうさんちょうめ"; romaji="hongosanchome"; shozoku="丸ノ内線"}; 
+{kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸ノ内線"} 
+] 
