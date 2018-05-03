d4be51d259d5f02b8ec8ed7749f6d1701e29a58a
numb86 <86numb@gmail.com>
2018-05-03 15:41:35 +0900

    14.3の演習問題を解いた。

---
+++ chapter14/q14-5.ml
@@ -0,0 +1,9 @@
+(* 目的：文字列のリスト lst を受け取り、その要素を前から順に全てつなげた文字列を返す *)
+(* concat: string list -> string *)
+let concat lst = let add str1 str2 = str1 ^ str2 in
+  List.fold_right add lst ""
+
+let test1 = concat [] = ""
+let test2 = concat ["a"] = "a"
+let test3 = concat ["あ"; "い"] = "あい"
+let test4 = concat ["あ"; ""; "いう"] = "あいう"
