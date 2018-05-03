13b7c1ea8fc71a444add362892692c9d922d60ed
numb86 <86numb@gmail.com>
2018-05-03 16:12:44 +0900

    q8-3.ml を追加。

---
+++ chapter8/q8-3.ml
@@ -0,0 +1,36 @@
+(* 人に関する情報を格納するレコード *) 
+type person_t = { 
+  name : string;	(* 名前 *) 
+  shincho : float;	(* 身長 *) 
+  taiju : float;	(* 体重 *) 
+  tsuki : int;		(* 誕生月 *) 
+  hi : int;		(* 誕生日 *) 
+  ketsueki : string;	(* 血液型 *) 
+} 
+ 
+let person1 = 
+  {name = "浅井"; 
+   shincho = 1.72; 
+   taiju = 58.5; 
+   tsuki = 9; 
+   hi = 17; 
+   ketsueki = "A" 
+} 
+ 
+let person2 = { 
+  name = "宮原"; 
+  shincho = 1.63; 
+  taiju = 55.0; 
+  tsuki = 6; 
+  hi = 30; 
+  ketsueki = "B" 
+} 
+ 
+let person3 = { 
+  name = "中村"; 
+  shincho = 1.68; 
+  taiju = 63.0; 
+  tsuki = 6; 
+  hi = 6; 
+  ketsueki = "O" 
+} 
